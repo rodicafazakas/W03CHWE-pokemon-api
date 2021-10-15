58215b965f9ea1c5a855d5d3d9bf5152c7366cfa
Rodica Fazakas <11833084+rodicafazakas@users.noreply.github.com>
2021-10-15 18:39:31 +0200

    Added initial files

---
+++ index.js
@@ -0,0 +1,7 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("pokemon.json");
+const port = process.env.PORT || 4000;
+
+server.use(router);
+server.listen(port);
