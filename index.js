f7597bb9bf478904f9e264e6b14b04d171b00204
Rodica Fazakas <11833084+rodicafazakas@users.noreply.github.com>
2021-10-17 21:15:16 +0200

    Run from index

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const server = jsonServer.create();
 const router = jsonServer.router("pokemon.json");
 const port = process.env.PORT || 4000;
 
+console.log("Running on port number: " + port);
 server.use(cors());
 server.use(router);
 server.listen(port);
